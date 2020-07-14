7a4e5660424ac3df122fa15fe9a92a60ef9ae0ed
Patrick José <patrick.amaral@toroinvestimentos.com.br>
2020-07-14 03:51:39 -0300

    Console logging for posts on the API

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using SmartStocksImporter.Business;
 using SmartStocksImporter.Models;
 
@@ -12,11 +13,15 @@ namespace SmartStocksImporter
 
             var FundList = await _fundsImporterBusiness.GetFundsRanking();
 
+            Console.WriteLine("Sending data to Smart Stocks API...");
+
             foreach (ImportFund f in FundList)
             {
                 await _fundsImporterBusiness.Import(f, "fund");
                 await _fundsImporterBusiness.Import(await _fundsImporterBusiness.GetFundWallet(f.FundName), "wallet");
             }
+            
+            Console.WriteLine("Done!");
         }
     }
 }
